649e639b07d2d8617f34e1daab39a21b61f06176
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2026-02-28 18:37:57 +0100

    msg

--- src/data.js
+++ src/data.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://globalprimalmarket-api.vercel.app/api";
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "https://globalprimalmarket-api.vercel.app/api";
+// export const BASE_URL = "http://localhost:8000/api";
